fb681b7647573274a14bd8cb54f1209c365fa611
imen-yaich <68538536+imen-yaich@users.noreply.github.com>
2020-08-21 16:30:29 +0200

    Update server.js

--- result/server.js
+++ result/server.js
@@ -24,7 +24,7 @@ io.sockets.on('connection', function (socket) {
 });
 
 var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: 'postgres://postgres:postgres@10.128.1.199/postgres'
 });
 
 async.retry(
